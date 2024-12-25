eb01d97f7a3acebe5bbdd6d86e49ce08644ff5c0
mennamassri <mennarashad155@gmail.com>
2024-12-25 15:07:26 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Calculator from "./Calculator";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1 style={{ textAlign: "center", fontSize: "28px", margin: "20px 0" }}>
+       
+      </h1>
+      <Calculator />
+    </div>
+  );
+};
+
+export default App;
